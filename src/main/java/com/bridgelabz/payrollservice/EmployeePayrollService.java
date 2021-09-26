0fb2beffe60aa65bd1c4f38fdfb151a01a2afca0
sushant2931 <sushantlad8888@gmail.com>
2021-09-26 23:42:50 +0530

    ADDED : Welcome In Emplyoee Payroll Service Program

---
+++ src/main/java/com/bridgelabz/payrollservice/EmployeePayrollService.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.payrollservice;
+
+public class EmployeePayrollService {
+    public static void main(String[] args) {
+        System.out.println("....Welcome on Emplyoee Payroll Service Program ......");
+    }
+}
